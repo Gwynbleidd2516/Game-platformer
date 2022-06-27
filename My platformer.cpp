da73f63fd165b71fcda1e7161fe20e2b9a85e0de
Gwynbleidd2516 <106027995+Gwynbleidd2516@users.noreply.github.com>
2022-06-27 15:29:59 +0300

    Add files via upload

--- My platformer.cpp
+++ My platformer.cpp
@@ -1,7 +1,7 @@
 ﻿#include <SFML/Graphics.hpp>
 #include<iostream>
-#include"Object.h"
-#include"SpriteSet.h"
+#include"Vyaz/Object.h"
+#include"Vyaz/SpriteSet.h"
 using namespace std;
 using namespace sf;
 
@@ -10,24 +10,31 @@ int main()
 	sf::RenderWindow window(sf::VideoMode(1440, 800), "SFML works!", Style::Close);
 	window.setFramerateLimit(60);
 	sf::CircleShape shape(50.f);
-	
+
 	Object Wall;
 	Wall.setObject(Vector2f(800, 20), Color::Red);
 	Wall.setPosition(200, 600);
 
 	Object Wall2;
-	Wall2.setObject(Vector2f(20, 800), Color::Red);
+	Wall2.setObject(Vector2f(20, 600), Color::Red);
 	Wall2.setPosition(1000, 20);
 
 	Object Cube;
-	Cube.setObject(Vector2f(100, 100), Color::Red);
+	Cube.setObject(Vector2f(100, 100), Color::Blue);
 	Cube.collision(Wall.getGlobalBounds(), true);
 	Cube.collision(Wall2.getGlobalBounds(), true);
 
-	SpriteSet Player;
-	Player.setSprites("Textures/DemonWalking.png", IntRect(0, 0, 32, 32), 8, StyleOfSet::cycle);
-	Player.setScale(3, 3);
-	float PlayerSpeed = 10;
+	SpriteSet PlayerRight;
+	PlayerRight.setSprites("Textures/DemonWalking.png", IntRect(0, 0, 32, 32), 8, StyleOfSet::cycle);
+	PlayerRight.setScale(3, 3);
+
+	SpriteSet PlayerLeft;
+	PlayerLeft.setSprites("Textures/DemonWalking2.png", IntRect(0, 0, 32, 32), 8, StyleOfSet::cycle);
+	PlayerLeft.setScale(3, 3);
+
+	SpriteSet PlayerRightNow;
+	PlayerRightNow.setSprites("Textures/DemonWalking.png", IntRect(0, 0, 32, 32), 8, StyleOfSet::OnlySprite);
+	PlayerRightNow.setScale(3, 3);
 
 	Object ObPlayer;
 	ObPlayer.setObject(Vector2f(32, 32), Color::Transparent);
@@ -35,16 +42,40 @@ int main()
 	ObPlayer.collision(Wall.getGlobalBounds(), true);
 	ObPlayer.collision(Wall2.getGlobalBounds(), true);
 	ObPlayer.setPosition(500, 400);
-	int scale = 1;
+	float PlayerSpeed = 10;
 
 	Texture txBackGround;
 	txBackGround.loadFromFile("Textures/BackGround.png");
 	Sprite spBackGround(txBackGround);
 
+	View Camera;
+
+	//////////////////////////////////////////////////
+	SpriteSet ZombeStand;
+	ZombeStand.setSprites("Textures/ZombeStand.png", IntRect(0, 0, 32, 32), 2, StyleOfSet::OnlySprite);
+	ZombeStand.setScale(3, 3);
+
+	SpriteSet ZombeWalkingLeft;
+	ZombeWalkingLeft.setSprites("Textures/ZombeWalkingLeft.png", IntRect(0, 0, 32, 32), 4, StyleOfSet::cycle);
+	ZombeWalkingLeft.setScale(3, 3);
+
+	SpriteSet ZombeWalkingRight;
+	ZombeWalkingRight.setSprites("Textures/ZombeWalkingRight.png", IntRect(0, 0, 32, 32), 4, StyleOfSet::cycle);
+	ZombeWalkingRight.setScale(3, 3);
+
+	Object Zombe;
+	Zombe.setObject(Vector2f(32, 32), Color::Transparent);
+	Zombe.setScale(3, 3);
+	Zombe.collision(Wall.getGlobalBounds(), true);
+	Zombe.collision(Wall2.getGlobalBounds(), true);
+	Zombe.setSprite(ZombeStand.getSprite());
+	bool purse = false;
 
 	while (window.isOpen())
 	{
-		ObPlayer.setSprite(Player.getSprite());
+		Camera.setCenter(ObPlayer.getPosition());
+		Camera.setSize(720, 400);
+		
 		ObPlayer.setGravitation(false);
 		Cube.setGravitation(false);
 
@@ -55,58 +86,88 @@ int main()
 				window.close();
 
 			if (Keyboard::isKeyPressed(Keyboard::Escape)) window.close();
-			if (event.type == Event::MouseButtonPressed) {
 
-				if (Mouse::isButtonPressed(Mouse::Button::Left)) {
-					//cout << event.key.code << endl;
-					if (Mouse::getPosition().x >= Cube.getPosition().x && Mouse::getPosition().y >= Cube.getPosition().y && Mouse::getPosition().x <= Cube.getSize().x && Mouse::getPosition().y <= Cube.getSize().y) {
-						Cube.setPosition(Mouse::getPosition().x, Mouse::getPosition().y);
-
-					}
-
-				}
-			}
-			
 			if (event.type == Event::KeyPressed) {
 				//cout << event.key.code << endl;
 
 				if (Keyboard::isKeyPressed(Keyboard::D)) {
-					scale = 1;
+					
 
 					if (ObPlayer.getPosition().x < window.getSize().x - ObPlayer.getSize().x * 3) {
-						Player.spritesActivation(100, true);
+						ObPlayer.setSprite(PlayerRight.getSprite());
+						PlayerRight.spritesActivation(100, true);
+						PlayerRight.setScale(3, 3);
 						ObPlayer.move(+PlayerSpeed, 0);
 
 					}
 				}
-				if (Keyboard::isKeyPressed(Keyboard::A)) {
-					scale = -1;
+				else if (Keyboard::isKeyPressed(Keyboard::A)) {
+					
 
-					if (ObPlayer.getPosition().x > ObPlayer.getSize().x * 3) {
-						Player.spritesActivation(100, true);
+					if (ObPlayer.getPosition().x > 0) {
+						ObPlayer.setSprite(PlayerLeft.getSprite());
+						PlayerLeft.spritesActivation(100, true);
+						PlayerLeft.setScale(3, 3);
 						ObPlayer.move(-PlayerSpeed, 0);
 
 					}
 				}
-				if (Keyboard::isKeyPressed(Keyboard::W)) ObPlayer.move(0, -PlayerSpeed);
-				if (Keyboard::isKeyPressed(Keyboard::S)) ObPlayer.move(0, PlayerSpeed);
+
+				if (Keyboard::isKeyPressed(Keyboard::W)) {
+					ObPlayer.setSprite(PlayerRight.getSprite());
+					PlayerRight.spritesActivation(100, false);
+					PlayerRight.setScale(3, 3);
+					ObPlayer.move(0, -PlayerSpeed);
+				}
+				else if (Keyboard::isKeyPressed(Keyboard::S)) {
+					ObPlayer.setSprite(PlayerLeft.getSprite());
+					PlayerLeft.spritesActivation(100, false);
+					PlayerLeft.setScale(3, 3);
+					ObPlayer.move(0, PlayerSpeed);
+				}
+				
 			}
+			
 		}
 
-		ObPlayer.setScale(scale * 3, 3);
-		Player.setScale(scale * 3, 3);
+		ObPlayer.setScale(3, 3);
 
-		//cout << ObPlayer.isCollision(Cube.getGlobalBounds()) << endl;
-		Cube.pursue(ObPlayer.getPosition(), 2);
+//////////////////////////////////////////////////////////////////////////////////////////////////////		
 
-		cout << Cube.isCollisionLine(ObPlayer) << endl;
+		if (ObPlayer.getDistanceTo(Zombe.getPosition()) >= 40 &&ObPlayer.getDistanceTo(Zombe.getPosition()) <= 400 && ObPlayer.isCollisionLine(Zombe) == false) {
+			Zombe.pursue(ObPlayer.getPosition(), 2);
+			if (Zombe.getPosition().x > ObPlayer.getPosition().x) {
+				Zombe.setSprite(ZombeWalkingLeft.getSprite());
+				ZombeWalkingLeft.spritesActivation(150, true);
+			}
+			else {
+				Zombe.setSprite(ZombeWalkingRight.getSprite());
+				ZombeWalkingRight.spritesActivation(150, true);
+			}
+			purse = true;
+		}
+		else {
+			Zombe.setSprite(ZombeStand.getSprite());
+			if (Zombe.getPosition().x > ObPlayer.getPosition().x) {
+				ZombeStand.setSpriteNumber(0);
+			}
+			else {
+				ZombeStand.setSpriteNumber(1);
+			}
+			purse = false;
+		}
+		cout << purse << endl;
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+		
+		window.setView(Camera);
 		window.clear();
 		window.draw(spBackGround);
 		Wall.render(window, false);
 		Wall2.render(window, false);
-		Cube.render(window, false);
+		//Cube.render(window, false);
 		ObPlayer.render(window, true);
+		Zombe.render(window, true);
 		window.display();
 	}
 
